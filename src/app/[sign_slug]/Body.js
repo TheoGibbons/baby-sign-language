e8d846de55a8754d9085ce5ef1430fcfff21f8f5
Theo Gibbons <theo_112@hotmail.com>
2026-07-09 16:11:12 -0700

    Fix dependency in useEffect to include 'sign' for proper state updates

--- src/app/[sign_slug]/Body.js
+++ src/app/[sign_slug]/Body.js
@@ -77,7 +77,7 @@ export default function Body({signs, signSlug}) {
         setSign(randomSign);
       }
     }
-  }, [signs, signSlug]);
+  }, [sign, signs, signSlug]);
 
   useEffect(() => {
     if (sign) {
